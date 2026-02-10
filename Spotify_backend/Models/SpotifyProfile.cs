ac05b2ecdafa604419cbbd94193baa08294d622d
Tobias Lauritsen <tvla23@student.du>
2026-02-10 22:41:37 +0100

    added feature to fetch profile infomation

---
+++ Spotify_backend/Models/SpotifyProfile.cs
@@ -0,0 +1,6 @@
+﻿namespace Spotify_backend.Models
+{
+    public class Class
+    {
+    }
+}
--- Spotify_backend/obj/Debug/net8.0/Spotify_backend.AssemblyInfo.cs
+++ Spotify_backend/obj/Debug/net8.0/Spotify_backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Spotify_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c3fe7fd5e0fdb4ddce2acac5c649ccbf991e310")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7e0ef2e98116a6ea15a2ec250c080a94d03bfb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Spotify_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Spotify_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
