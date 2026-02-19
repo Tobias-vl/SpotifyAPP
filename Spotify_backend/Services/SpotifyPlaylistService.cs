87984d792f6992fa81a5ad58e12d56d3c656a306
Tobias Lauritsen <tobias.lauritsen@hotmail.com>
2026-02-19 21:56:15 +0100

    forget to save files so new api calls

--- Spotify_backend/Services/SpotifyPlaylistService.cs
+++ Spotify_backend/Services/SpotifyPlaylistService.cs
@@ -1,5 +1,7 @@
-﻿using Spotify_backend.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using Spotify_backend.Models;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Text.Json;
 
 namespace Spotify_backend.Services
@@ -17,7 +19,7 @@ namespace Spotify_backend.Services
 
         public async Task<Playlist> GetPlaylists(string accessToken, string userId)
         {
-                       if (accessToken == null)
+            if (accessToken == null)
             {
                 throw new Exception("accessToken is null");
             }
@@ -30,7 +32,7 @@ namespace Spotify_backend.Services
                 throw new Exception($"Spotify API error ({response.StatusCode}): {error}");
             }
             var json = await response.Content.ReadAsStringAsync();
-            var ListOfPlaylist= JsonSerializer.Deserialize<Playlist>(json, new JsonSerializerOptions
+            var ListOfPlaylist = JsonSerializer.Deserialize<Playlist>(json, new JsonSerializerOptions
             {
                 PropertyNameCaseInsensitive = true
             });
@@ -43,6 +45,78 @@ namespace Spotify_backend.Services
             return ListOfPlaylist;
 
         }
+
+
+        public async Task<PlaylistItems> GetPlaylistItems(string playlist_id, string accessToken)
+        {
+            _http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            var response = await _http.GetAsync($"https://api.spotify.com/v1/playlists/{playlist_id}/items");
+            if (!response.IsSuccessStatusCode)
+            {
+                string error = await response.Content.ReadAsStringAsync();
+                throw new Exception($"Spotify API error ({response.StatusCode}): {error}");
+            }
+            var json = await response.Content.ReadAsStringAsync();
+            var playlistItems = JsonSerializer.Deserialize<PlaylistItems>(json, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+            if (playlistItems == null)
+            {
+                throw new Exception("Playlist list is empty");
+            }
+
+            return playlistItems;
+
+        }
+
+        public async Task<string> DeletePlaylistItems(string playlist_id, string accessToken)
+        {
+            _http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            var response = await _http.DeleteAsync($"https://api.spotify.com/v1/playlists/{playlist_id}/items");
+            if (!response.IsSuccessStatusCode)
+            {
+                string error = await response.Content.ReadAsStringAsync();
+                throw new Exception($"Spotify API error ({response.StatusCode}): {error}");
+            }
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> CreatePlaylist(string playlistName, string accessToken, string userId)
+        {
+            _http.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", accessToken);
+
+            var json = new
+            {
+                name = playlistName,
+
+            };
+
+            var content = new StringContent(
+                JsonSerializer.Serialize(json),
+                Encoding.UTF8,
+                "application/json"
+            );
+
+            var response = await _http.PostAsync(
+                $"https://api.spotify.com/v1/users/{userId}/playlists",
+                content
+            );
+
+            if (!response.IsSuccessStatusCode)
+            {
+                var error = await response.Content.ReadAsStringAsync();
+                throw new Exception($"Spotify API error ({response.StatusCode}): {error}");
+            }
+
+            var responseJson = await response.Content.ReadAsStringAsync();
+            return responseJson;
+        }
+
+
+
 
     }
 }
